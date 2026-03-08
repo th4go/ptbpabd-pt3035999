c65b3411c4eb321743b596590e922ba66e314335
thiptv <t426lolk@gmail.com>
2026-03-08 17:19:03 -0300

    ATIVIDADE Aula 03

---
+++ consulta.sql
@@ -0,0 +1,12 @@
+SELECT COUNT(*) FROM advisor; -- 2.000
+SELECT COUNT(*) FROM classroom; -- 30
+SELECT COUNT(*) FROM course; -- 200
+SELECT COUNT(*) FROM department; -- 20
+SELECT COUNT(*) FROM instructor; -- 50
+SELECT COUNT(*) FROM prereq; -- 100
+SELECT COUNT(*) FROM section; -- 100
+SELECT COUNT(*) FROM student; -- 2.000
+SELECT COUNT(*) FROM takes; -- 30.000
+SELECT COUNT(*) FROM teaches; -- 100
+SELECT COUNT(*) FROM time_slot; -- 20
+ 
